f9306972f78c6d4cf3f6c9a6f9b7b8d32ab28fc8
Zhengfa DANG <dantifer@gmail.com>
2015-04-28 23:47:45 +0800

    make login view the entry view

--- zSMTH/ViewController/LoginViewController.m
+++ zSMTH/ViewController/LoginViewController.m
@@ -79,7 +79,7 @@
     setting.bSavePassword = self.switchSavePassword.on;
     setting.bAutoLogin = self.switchAutoLogin.on;
     
-//    NSLog(@"%@", setting);
+    NSLog(@"%@", setting);
     
     [helper login:[self.editUsername text] password:[self.editPassword text]];
 }
@@ -110,6 +110,23 @@
 - (IBAction)cancel:(id)sender {
     // 回到rootView
     [self.navigationController popToRootViewControllerAnimated:YES];
+    [self exitApplication];
+}
+
+- (void)exitApplication {
+    [UIView beginAnimations:@"exitApplication" context:nil];
+    [UIView setAnimationDuration:0.5];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationTransition:UIViewAnimationTransitionFlipFromRight forView:self.view.window cache:NO];
+    [UIView setAnimationDidStopSelector:@selector(animationFinished:finished:context:)];
+    self.view.window.bounds = CGRectMake(0, 0, 0, 0);
+    [UIView commitAnimations];
+}
+
+- (void)animationFinished:(NSString *)animationID finished:(NSNumber *)finished context:(void *)context {
+    if ([animationID compare:@"exitApplication"] == 0) {
+        exit(0);
+    }
 }
 
 @end
